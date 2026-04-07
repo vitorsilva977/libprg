1b7992088ea54e6704477efb6a71763b09bbe188
vitor <vitor.z12@aluno.ifsc.edu.br>
2026-04-07 09:09:07 -0300

    função adicionar pronta

--- libprg/src/libprg/lista.c
+++ libprg/src/libprg/lista.c
@@ -26,3 +26,28 @@ lista_t* criar_lista (int capacidade, bool ordenada) {
     lista->ordenada = ordenada;
     return lista;
 }
+
+void adicionar_a_lista(lista_t* lista, int valor) {
+    if (lista->tamanho >= lista->capacidade) {
+        int nova_capacidade = lista->capacidade * 2;
+
+        int* temp = (int*) realloc(lista->elementos, sizeof(int) * nova_capacidade);
+        if (temp == NULL) return; // falhou, não altera nada
+
+        lista->elementos = temp;
+        lista->capacidade = nova_capacidade;
+    }
+
+    if (lista->ordenada && lista->tamanho > 0) {
+        int i = lista->tamanho - 1;
+        while (i >= 0 && valor < lista->elementos[i]) {
+            lista->elementos[i + 1] = lista->elementos[i];
+            i--;
+        }
+        lista->elementos[i + 1] = valor;
+    } else {
+        lista->elementos[lista->tamanho] = valor;
+    }
+
+    lista->tamanho++;
+}
