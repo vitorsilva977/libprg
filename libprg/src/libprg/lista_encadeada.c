720d9c0fbe48ad8c392d6448861602a3510d65e6
vitor <vitor.z12@aluno.ifsc.edu.br>
2026-05-05 17:11:10 -0300

    alteração fçs lista_encadeada.c

--- libprg/src/libprg/lista_encadeada.c
+++ libprg/src/libprg/lista_encadeada.c
@@ -7,26 +7,39 @@ typedef struct no {
     no_t* proximo;
 }no_t;
 
-no_t* criar_lista_encadeada(int dado) {
+typedef struct lista_encadeada {
+    no_t* inicio;
+    bool ordenada;
+}lista_encadeada_t;
+
+no_t* criar_no(int dado) {
     no_t* no = malloc(sizeof(no_t));
     no->dado = dado;
     no->proximo = NULL;
 
     return no;
 }
-void inserir_encadeada(no_t** inicio, int dado) {
-    no_t* novo = criar_lista_encadeada(dado);
-    novo->proximo = *inicio;
-    *inicio = novo;
-}
-bool remover_encadeada(no_t** inicio, int dado) {
 
-    no_t* atual = *inicio;
+lista_encadeada_t* criar_lista_encadeada(bool* ordenada) {
+    lista_encadeada_t* lista = malloc(sizeof(lista_encadeada_t));
+    lista->inicio = NULL;
+    lista->ordenada = ordenada;
+
+    return lista;
+}
+void inserir_encadeada(lista_encadeada_t* lista, int dado) {
+    no_t* novo = criar_no(dado);
+    novo->proximo = lista->inicio;
+    lista->inicio = novo;
+}
+bool remover_encadeada(lista_encadeada_t* lista, int dado) {
+
+    no_t* atual = lista->inicio;
     no_t* anterior = NULL;
 
     while (atual != NULL) {
         if (atual->dado == dado) {
-            if (anterior == NULL) *inicio = atual->proximo;
+            if (anterior == NULL) lista->inicio = atual->proximo;
             else anterior->proximo = atual->proximo;
 
             free(atual);
@@ -39,19 +52,19 @@ bool remover_encadeada(no_t** inicio, int dado) {
     return false;
 }
 
-no_t* buscar_encadeada(no_t** inicio, int dado) {
-    no_t* atual = *inicio;
+no_t* buscar_encadeada(lista_encadeada_t* lista, int dado){
+    no_t* atual = lista->inicio;
 
     while (atual != NULL) {
         if (atual->dado == dado) return atual;
         atual = atual->proximo;
     }
-   return atual;
+   return NULL;
 }
 
-void destruir_encadeada(no_t** inicio) {
+void destruir_encadeada(lista_encadeada_t* lista) {
 
-    no_t* atual = *inicio;
+    no_t* atual = lista->inicio;
 
     while (atual != NULL) {
         no_t* proximo = atual->proximo;
