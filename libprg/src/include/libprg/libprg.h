56112be22059247b17f502f041fcb94ed691f145
vitor <vitor.z12@aluno.ifsc.edu.br>
2026-05-24 15:51:16 -0300

    arvore completa

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -3,64 +3,102 @@
 #define LIBPRG_LIBPRG_H
 /*  === PILHA ===*/
 typedef struct pilha pilha_t;
-pilha_t* criar_pilha(int capacidade);
-int empilhar_pilha(pilha_t* pilha, int valor);
-int desempilhar_pilha(pilha_t* pilha);
-int tamanho_pilha(pilha_t* pilha);
-int vazia_pilha(pilha_t* pilha);
-int destruir_pilha(pilha_t* pilha);
+
+pilha_t *criar_pilha(int capacidade);
+
+int empilhar_pilha(pilha_t *pilha, int valor);
+
+int desempilhar_pilha(pilha_t *pilha);
+
+int tamanho_pilha(pilha_t *pilha);
+
+int vazia_pilha(pilha_t *pilha);
+
+int destruir_pilha(pilha_t *pilha);
 
 /* == FILA == */
 typedef struct fila fila_t;
-fila_t* criar_fila(int capacidade);
-void enfileirar_fila(fila_t* fila, int valo);
-int desenfileirar_fila(fila_t* fila);
-int inicio_fila(fila_t* fila);
-int fim_fila(fila_t* fila);
-int vazia_fila(fila_t* fila);
-int tamanho_fila(fila_t* fila);
-int cheia_fila(fila_t* fila);
-int destruir_fila(fila_t* fila);
+
+fila_t *criar_fila(int capacidade);
+
+void enfileirar_fila(fila_t *fila, int valo);
+
+int desenfileirar_fila(fila_t *fila);
+
+int inicio_fila(fila_t *fila);
+
+int fim_fila(fila_t *fila);
+
+int vazia_fila(fila_t *fila);
+
+int tamanho_fila(fila_t *fila);
+
+int cheia_fila(fila_t *fila);
+
+int destruir_fila(fila_t *fila);
 
 // /* == LISTA  == */
- typedef struct lista lista_t;
- lista_t* criar_lista (bool ordenada);
+typedef struct lista lista_t;
+
+lista_t *criar_lista(bool ordenada);
+
 bool inserir(lista_t *lista, int elementos);
+
 int remover(lista_t *lista);
+
 int buscar(lista_t *lista, int elementos);
+
 int alterar(lista_t *lista, int elementoAntigo, int elementoNovo);
+
 int ordenar(lista_t *lista);
 
 
 /*   == LISTA_ENCADEADA == */
 typedef struct no no_t;
 typedef struct lista_encadeada lista_encadeada_t;
-no_t* criar_no(int dado);
-lista_encadeada_t* criar_lista_encadeada(bool* ordenada);
-void inserir_encadeada(lista_encadeada_t* lista, int dado);
-bool remover_encadeada(lista_encadeada_t* lista, int dado);
-no_t* buscar_encadeada(lista_encadeada_t* lista, int dado);
-void destruir_encadeada(lista_encadeada_t* lista);
+
+no_t *criar_no(int dado);
+
+lista_encadeada_t *criar_lista_encadeada(bool *ordenada);
+
+void inserir_encadeada(lista_encadeada_t *lista, int dado);
+
+bool remover_encadeada(lista_encadeada_t *lista, int dado);
+
+no_t *buscar_encadeada(lista_encadeada_t *lista, int dado);
+
+void destruir_encadeada(lista_encadeada_t *lista);
 
 /* == LISTA_ENCADEADA_DUPLA == */
 typedef struct nod nod_t;
-bool remover_encadeada_dupla(nod_t** inicio, int dado);
-void *destruir_encadeada_dupla(nod_t** inicio, int dado);
+bool remover_encadeada_dupla(nod_t **inicio, int dado);
+
+void *destruir_encadeada_dupla(nod_t **inicio, int dado);
 
 /*  == ALGORITMOS DE ORDENAÇÃO == */
-int* bubble_sort(int* vetor, int tamanho);
-int* merge_sort(int* vetor, int esquerda, int direita);
-int* merge(int* veitor, int esquerda,int meio, int direita);
-int particiona(int* vetor, int inicio, int fim);
-int* quicksort(int* vetor, int inicio, int fim);
+int *bubble_sort(int *vetor, int tamanho);
+
+int *merge_sort(int *vetor, int esquerda, int direita);
+
+int *merge(int *veitor, int esquerda, int meio, int direita);
+
+int particiona(int *vetor, int inicio, int fim);
+
+int *quicksort(int *vetor, int inicio, int fim);
+
 /*   == TABELA DE DISPERÇÃO ==   */
 typedef struct noh noh_t;
 typedef struct dicionario dicionario_t;
-int inserir_hash(dicionario_t* d, char *chave, int valor);
+int inserir_hash(dicionario_t *d, char *chave, int valor);
 
 /*  == ARVORE == */
 typedef struct no_arvore no_arvore_t;
-no_arvore_t* criar_no_arvore(int dado);
-no_arvore_t* adicionar_no_arvore(no_arvore_t* raiz, int dado);
+no_arvore_t *criar_no_arvore(int dado);
+no_arvore_t *adicionar_no_arvore(no_arvore_t *raiz, int dado);
+no_arvore_t *remover_arvore(no_arvore_t *raiz, int valor);
+no_arvore_t *menor_no(no_arvore_t *no);
+void travessia_pre_ordem(no_arvore_t *raiz);
 void travessia_emordem(no_arvore_t *raiz);
+void travessia_posordem(no_arvore_t *raiz);
+
 #endif
--- libprg/src/libprg/arvore.c
+++ libprg/src/libprg/arvore.c
@@ -19,7 +19,6 @@ no_arvore_t *criar_no_arvore(int dado) {
 }
 
 no_arvore_t *adicionar_no_arvore(no_arvore_t *raiz, int dado) {
-
     if (raiz == NULL) {
         return criar_no_arvore(dado);
     }
@@ -35,14 +34,84 @@ no_arvore_t *adicionar_no_arvore(no_arvore_t *raiz, int dado) {
     return raiz;
 }
 
-//remover
-//destruir
+no_arvore_t *remover_arvore(no_arvore_t *raiz, int valor) {
+    if (raiz == NULL)
+        return NULL;
+
+    if (valor < raiz->dado)
+        raiz->esquerda = remover_arvore(raiz->esquerda, valor);
+
+    else if (valor > raiz->dado)
+        raiz->direita = remover_arvore(raiz->direita, valor);
+
+    else {
+        if (raiz->esquerda == NULL && raiz->direita == NULL) {
+            free(raiz);
+            return NULL;
+        }
+
+        if (raiz->esquerda == NULL) {
+            no_arvore_t *temp = raiz->direita;
+            free(raiz);
+            return temp;
+        }
+
+        if (raiz->direita == NULL) {
+            no_arvore_t *temp = raiz->esquerda;
+            free(raiz);
+            return temp;
+        }
+
+        no_arvore_t *temp = menor_no(raiz->direita);
+
+        raiz->dado = temp->dado;
+
+        raiz->direita = remover_arvore(raiz->direita, temp->dado);
+    }
+
+    return raiz;
+}
+
+no_arvore_t *menor_no(no_arvore_t *no) {
+    no_arvore_t *atual = no;
+
+    while (atual != NULL && atual->esquerda != NULL) {
+        atual = atual->esquerda;
+    }
+
+    return atual;
+}
+
+void destruir_arvore(no_arvore_t *raiz) {
+    if (raiz == NULL) return;
+    destruir_arvore(raiz->esquerda);
+    destruir_arvore(raiz->direita);
+    free(raiz);
+}
+
+void travessia_pre_ordem(no_arvore_t *raiz) {
+
+    if (raiz == NULL)
+        return;
+
+    printf("%d ", raiz->dado);
+    travessia_pre_ordem(raiz->esquerda);
+    travessia_pre_ordem(raiz->direita);
+}
+
 void travessia_emordem(no_arvore_t *raiz) {
-    if (raiz !=NULL) {
+    if (raiz != NULL) {
         travessia_emordem(raiz->esquerda);
-        printf("%d ",raiz->dado);
+        printf("%d ", raiz->dado);
         travessia_emordem(raiz->direita);
     }
 }
-//travessia_emordem
-//travessia_posordem
+
+void travessia_posordem(no_arvore_t *raiz) {
+    if (raiz == NULL)
+        return;
+
+    travessia_posordem(raiz->esquerda);
+    travessia_posordem(raiz->direita);
+    printf("%d ", raiz->dado);
+}
